5182ed531cc6a821499f92d1fa484218cb385841
shaun <shaun.19980121@gmail.com>
2025-12-12 17:41:41 +0800

    UPDATE main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AppProvider } from './context/AppContext.tsx'
+import { AppProviders } from './context/IndexContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProvider>
+    <AppProviders>
       <App />
-    </AppProvider>
+    </AppProviders>
   </StrictMode>,
 )
